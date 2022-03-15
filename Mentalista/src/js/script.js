a6ccd08e050b6c7d7ecfa5fe5d019d552be258fc
mnnobre <mn.nobre@outlook.com>
2022-03-15 01:54:22 -0300

    add mentalista dia 3

---
+++ Mentalista/src/js/script.js
@@ -0,0 +1,16 @@
+var numeroSecreto = parseInt(Math.random() * 11);
+
+function Chutar() {
+    var chute = parseInt(document.getElementById("valor").value);
+    var elementoResultado = document.getElementById("resultado");
+
+    if (chute == numeroSecreto) {
+        elementoResultado.innerHTML = "Você Acertou";
+        console.log("Você acertou");
+    } else if (chute > 10 || chute < 0) {
+        elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10";
+    } else {
+        elementoResultado.innerHTML =
+            "Você errou, " + "o numero secreto era " + numeroSecreto;
+    }
+}
